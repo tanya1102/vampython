1510f908876fa656a8a581d8f4a941725f4ca887
tanya1102 <tanupra321@gmail.com>
2024-09-30 20:51:00 +0530

    Delete set.py

--- set.py
+++
@@ -1,7 +0,0 @@
-#It can store multiple data.
-#It is unordered,no duplicate and unchangeable.
-#Curly braces
-
-mySet = {"Priya","Tanu","Veer"}
-print(mySet)
-#mySet[0] = "Nonu" This will not work as it is unchangeable. 
